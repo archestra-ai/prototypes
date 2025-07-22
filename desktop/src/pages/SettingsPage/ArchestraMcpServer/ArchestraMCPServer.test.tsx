2d2902c721bf02727e13d8623f843ef7dc39affb
Joey Orlando <joseph.t.orlando@gmail.com>
2025-07-22 10:54:48 +0100

    wip

--- desktop/src/pages/SettingsPage/ArchestraMcpServer/ArchestraMCPServer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import ArchestraMcpServer from './';
-
-describe('ArchestraMcpServer', () => {
-  it('renders the ArchestraMcpServer component', () => {
-    render(<ArchestraMcpServer />);
-  });
-});
