d37cf384f494633f2af9132aecaef7d2981988d4
Ildar Iskhakov <ildar6v@gmail.com>
2025-08-04 18:35:39 +0100

    fix: correct import

--- desktop_app/src/backend/server/index.ts
+++ desktop_app/src/backend/server/index.ts
@@ -1,5 +1,5 @@
+import cors from '@fastify/cors';
 import fastify from 'fastify';
-import cors from 'fastify/cors';
 
 import config from '@backend/config';
 import chatRoutes from '@backend/server/plugins/chat';
