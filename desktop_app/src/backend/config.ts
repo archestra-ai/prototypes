f527263c2e165e214bd1e3707ce2c2e3efe19193
Joey Orlando <joseph.t.orlando@gmail.com>
2025-08-04 20:17:04 -0400

    use `archestra-public` GAR registry

--- desktop_app/src/backend/config.ts
+++ desktop_app/src/backend/config.ts
@@ -23,6 +23,6 @@ export default {
   sandbox: {
     baseDockerImage:
       process.env.MCP_BASE_DOCKER_IMAGE ||
-      'europe-west1-docker.pkg.dev/friendly-path-465518-r6/archestra/mcp-server-base:0.0.1',
+      'europe-west1-docker.pkg.dev/friendly-path-465518-r6/archestra-public/mcp-server-base:0.0.1',
   },
 };
